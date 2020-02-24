2bb8f54227e27c0ed8acdd475fe2ebf6eadc8dc6
Justin Huang <justin9.ij@gmail.com>
2020-02-24 15:02:19 -0500

    Add files via upload

---
+++ src/pkj/Class.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package pkj;
+
+/**
+ * @author Justi
+ *
+ */
+public class Class {
+
+	/**
+	 * 
+	 */
+	public Class() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
